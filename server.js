c5c9bd6940295adc431866b60d6adfb6f3a4546d
snowquirk <snowquirk@gmail.com>
2021-03-21 14:11:08 +1100

    test

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const PORT = process.env.PORT;
 
 const server = app.listen(
   PORT,
-  console.log(`Server is running on ${PORT} `.yellow.bold)
+  console.log(`Server is running in ${PORT} `.yellow.bold)
 );
 
 //Handle unhandled promise rejections
